b308b279a87d6e1122ba91c741103367428ab557
manufrancis999 <31932752+manufrancis999@users.noreply.github.com>
2022-03-30 16:25:33 +0530

    Added image encryption library to phoger.py

--- phoger.py
+++ phoger.py
@@ -5,7 +5,7 @@ from res.lib import enclib
 from res.lib import utils
 import hashlib,json
 
-#For Pillow
+# For Pillow
 from PIL  import Image
 
 #For Arguments parsing
@@ -37,32 +37,83 @@ def to_md5(password):
 encrypted_msg = enclib.encrypt_AES_256_GCM(msg, to_md5(password))
 
 # The length of the string is attached to the beginning 32 bits of the string
-msg_to_encode = '{:032b}'.format(len(encrypted_msg)) + encrypted_msg
+msg_to_encode = '{:032b}'.format(len(encrypted_msg)) + encrypted_msg + '00000000'
 
+# Pillow Integration
 
-# Pillow integration
-im=Image.open('src.png')
-img=im.load()
+##################################################
+#Encoding to image
+##################################################
+
+im = Image.open('src.png')
+img = im.load()
 print(im.size)
-[xs,ys]=im.size
+[xs,ys] = im.size
 
 # Checking if the image size is enough to write the message
-if(xs*ys >= len(msg_to_encode)):
+len_msg = len(msg_to_encode) - 8
+
+if(xs*ys*4 >= len_msg):
     #Encode the message to image
+    k = 0 #index for msg
+    while(k <= len_msg):
+        for i in range(ys):
+            for j in range(xs):
+                (r,g,b,a) = img[j,i]
+                # k th
+                if(msg_to_encode[k] == 0):
+                    if(r%2 == 1):
+                        r -= 1
+                else:
+                    if(r%2 == 0):
+                        r += 1
+                # k+1 th
+                if(msg_to_encode[k+1] == 0):
+                    if(g%2 == 1):
+                        g -= 1
+                else:
+                    if(g%2 == 0):
+                        g += 1        
+                # k+2 th
+                if(msg_to_encode[k+2] == 0):
+                    if(b%2 == 1):
+                        b -= 1
+                else:
+                    if(b%2 == 0):
+                        b += 1       
+                # k+3 th         
+                if(msg_to_encode[k+3] == 0):
+                    if(a%2 == 1):
+                        a -= 1
+                else:
+                    if(a%2 == 0):
+                        a += 1
+                img[j,i] = (r,g,b,a)
+                k += 4        
+                if(k >len_msg):
+                    break
+            if(k>len_msg):
+                break        
 
 
-
-
-  #width*height
+#width*height
+else:
+    print("[-] The resoulution is not enough")  
 
 """
 # Examine every pixel in im
-  for x in range(0,xs):
-     for y in range(0,ys):
+for x in range(0,xs):
+    for y in range(0,ys):
         #get the RGB color of the pixel
         img[x,y] = [r,g,b]
 """
 im.save('output.png')
+ 
+##################################################
+# Decoding from image
+##################################################
+
+
 #######################################################################
 #Test and Trial Printing section below
 #######################################################################
