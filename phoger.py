eefe669ce7ad38ed88e953f75735e7d5ac92e01b
manufrancis999 <31932752+manufrancis999@users.noreply.github.com>
2022-04-04 14:54:02 +0530

    Image encoding and decoding trial

--- phoger.py
+++ phoger.py
@@ -10,7 +10,7 @@ from PIL  import Image
 
 #For Arguments parsing
 import argparse
-
+import time
 
 
 # Parsing the arguments from command line
@@ -51,73 +51,97 @@ print(im.size)
 [xs,ys] = im.size
 
 # Checking if the image size is enough to write the message
-len_msg = len(msg_to_encode) - 8
-
+len_msg = (len(msg_to_encode) - 8)
 if(xs*ys*4 >= len_msg):
     #Encode the message to image
     k = 0 #index for msg
-    while(k <= len_msg):
-        for i in range(ys):
-            for j in range(xs):
-                (r,g,b,a) = img[j,i]
-                # k th
-                if(msg_to_encode[k] == 0):
-                    if(r%2 == 1):
-                        r -= 1
-                else:
-                    if(r%2 == 0):
-                        r += 1
-                # k+1 th
-                if(msg_to_encode[k+1] == 0):
-                    if(g%2 == 1):
-                        g -= 1
-                else:
-                    if(g%2 == 0):
-                        g += 1        
-                # k+2 th
-                if(msg_to_encode[k+2] == 0):
-                    if(b%2 == 1):
-                        b -= 1
-                else:
-                    if(b%2 == 0):
-                        b += 1       
-                # k+3 th         
-                if(msg_to_encode[k+3] == 0):
-                    if(a%2 == 1):
-                        a -= 1
-                else:
-                    if(a%2 == 0):
-                        a += 1
-                img[j,i] = (r,g,b,a)
-                k += 4        
-                if(k >len_msg):
-                    break
-            if(k>len_msg):
-                break        
+    for i in range(ys):
+        for j in range(xs):
+            (r,g,b,a) = img[j,i]
+            # k th
+            if(msg_to_encode[k] == 0):
+                if((r%2) == 1):
+                    print("Bef",r)
+                    r -= 1
+                    print("Aft",r)
+            else:
+                if((r%2) == 0):
+                    print("Bef",r)
+                    r += 1
+                    print("Aft",r)
+            # k+1 th
+            if(msg_to_encode[k+1] == 0):
+                if((g%2) == 1):
+                    g -= 1
+            else:
+                if((g%2) == 0):
+                    g += 1        
+            # k+2 th
+            if(msg_to_encode[k+2] == 0):
+                if((b%2) == 1):
+                    b -= 1
+            else:
+                if((b%2) == 0):
+                    b += 1       
+            # k+3 th         
+            if(msg_to_encode[k+3] == 0):
+                if((a%2) == 1):
+                    a -= 1
+            else:
+                if((a%2) == 0):
+                    a += 1
+            k += 4        
+            if(k >len_msg):
+                break
+        if(k > len_msg):
+            break        
 
 
 #width*height
 else:
     print("[-] The resoulution is not enough")  
 
-"""
-# Examine every pixel in im
-for x in range(0,xs):
-    for y in range(0,ys):
-        #get the RGB color of the pixel
-        img[x,y] = [r,g,b]
-"""
-im.save('output.png')
- 
+im.save('out.png')
+
 ##################################################
 # Decoding from image
 ##################################################
+
+im = Image.open('out.png')
+ximg = im.load()
+print(im.size)
+[xs,ys] = im.size
+
+len_str = ''
+for i in range(8):
+    (r,g,b,a) = ximg[i,0]
+    print("test",r,g,b,a)
+    len_str = (len_str + str(r%2) + str(g%2)+ str(b%2)+ str(a%2))
+
+print("\n\n",len_str) 
+print("if same ok = ",'{:032b}'.format(len(encrypted_msg)))
+x = ximg[1,0]
+print(x)  
+im.close()
 
 
 #######################################################################
 #Test and Trial Printing section below
 #######################################################################
+im = Image.open('src.png')
+img = im.load()
+print(im.size)
+[xs,ys] = im.size
 
+len_str = ''
+for i in range(8):
+    (r,g,b,a) = img[0,i]
+    len_str = (len_str + str(r%2) + str(g%2)+ str(b%2)+ str(a%2))
+
+print("\n\n",len_str) 
+x = img[0,1]
+print(x)  
+im.close()
 #print("Message = ", encrypted_msg, "  Password Hash=", to_md5(password))
 
 #print("len=",enclib.decrypt_AES_256_gcm(encrypted_msg,"abc"))
@@ -125,10 +149,11 @@ im.save('output.png')
 #print(enclib.decrypt_AES_256_gcm(encrypted_msg[0],m),"\n\n\n")
 
 #print(encrypted_msg[2],encrypted_msg[1],"\n\n\n", enclib.decrypt_AES_256_gcm(encrypted_msg[0],m))
-print(encrypted_msg,enclib.decrypt_AES_256_gcm(encrypted_msg,to_md5(password)),msg_to_encode)
+#print(encrypted_msg,enclib.decrypt_AES_256_gcm(encrypted_msg,to_md5(password)),msg_to_encode)
 
 #print(encrypted_msg[2],encrypted_msg[1],"\n\n\n", enclib.decrypt_AES_256_gcm(encrypted_msg[0],m))
 
 
 
 #print(encrypted_msg[1],"\n\n\n", enclib.decrypt_AES_256_gcm(encrypted_msg[0],m))
+print(0%2)
---
+++ res/lib/imglib.py
@@ -0,0 +1,6 @@
+# This Library is for adding support for different types of image files
+
+
+# For PNG files
+def png_img(msg_to_encode):
+   
